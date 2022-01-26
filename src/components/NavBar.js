3dfc8b1c798eade3d35e34ff8dbe5ab08a5720b4
manaqu98 <92610695+manaqu98@users.noreply.github.com>
2022-01-26 15:54:56 -0300

    Problem solved. Developing Landing.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import "../components/NavBar.css";
 import logo from '../logo.png';
 
-function navBar() {
+const NavBar = () => {
     return (
-    <div className="navBar-div">
+    <main className="navBar-div">
         <header className='head-container'>
         <img src={logo} className="logoimg-container" alt="logomusical" width="50px" height="50px"/>
-        <navBar className="navBar-container">
+        <div className="navBar-container">
             <ul> 
                 <li><a href="https://www.google.com">Inicio</a></li>
                 <li><a href="https://www.google.com">Ofertas</a></li>
@@ -14,10 +14,10 @@ function navBar() {
                 <li><a href="https://www.google.com">Amplificadores</a></li>
                 <li><a href="https://www.google.com">Pedales/Pedaleras</a></li>
             </ul>
-        </navBar>
+        </div>
         </header>
-    </div>
+    </main>
     );
 }
 
-export default navBar;
+export default NavBar;
