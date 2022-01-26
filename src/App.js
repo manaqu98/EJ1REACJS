527636b3fcae22b456e322e22c13d1366e6cf846
manaqu98 <92610695+manaqu98@users.noreply.github.com>
2022-01-26 15:04:02 -0300

    Correccion de programacion con error en GUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from '../src/logo.png';
-import './App.css';
-//import { editableInputTypes } from '@testing-library/user-event/dist/utils';
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <div className="app">
-      <header className='head-container'>
-      <img src={logo} className="logoimg-container" alt="logomusical" width="50px" height="50px"/>
-      <navBar className="navBar-container">
-        <ul> 
-          <li><a href="https://www.google.com">Inicio</a></li>
-          <li><a href="https://www.google.com">Ofertas</a></li>
-          <li><a href="https://www.google.com">Instrumentos</a></li>
-          <li><a href="https://www.google.com">Amplificadores</a></li>
-          <li><a href="https://www.google.com">Pedales/Pedaleras</a></li>
-        </ul>
-      </navBar>
-      </header>
+      <script src={NavBar}/>
     </div>
-  );
+);
 }
 
 export default App;
