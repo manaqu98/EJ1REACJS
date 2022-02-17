b0dd72d97a0e984bc87501c6df67592ff539587c
manaqu98 <92610695+manaqu98@users.noreply.github.com>
2022-02-17 18:27:04 -0300

    API Context Test Part2

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -5,10 +5,15 @@ export const CartContext = createContext();
 export const CartProvider = ({children}) => {
     const[cart, setCart] = useState([]);
 
-    const addOn = (details, value) => {
-        const item = {quantity: value, item: details.brand+` `+details.model, price: details.price*value};
+    const addOn = (details, value) => { 
+        const item = {id: details.id, quantity: value, item: details.brand+` `+details.model, price: details.price*value};
         setCart(currentState => [...currentState, item]);
     };
+    
+    /*const addOn = (details, value) => { 
+        const item = {id: details.id, quantity: value, item: details.brand+` `+details.model, price: details.price*value};
+        setCart(currentState => cart.map(currentState => (currentState.id === details.id ? null : [...currentState, item])));
+    }; -----> Intento con parametro de que no agregue repetidos*/
 
     console.log(cart);
 
