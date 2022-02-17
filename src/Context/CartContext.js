7c35751d78ef398f22fd2f929735a20cd932695a
manaqu98 <92610695+manaqu98@users.noreply.github.com>
2022-02-17 16:33:33 -0300

    Context Test Part.2

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -2,12 +2,18 @@ import React, {createContext, useState} from 'react';
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({children, details}) => {
     const[cart, setCart] = useState([]);
 
+    const addOn = (value) => {
+        console.log(`it works addOn`);
+        const item = {quantity: value, item: details.brand+` `+details.model, price: details.price*value};
+        setCart(currentState => [...currentState, item]);
+    };
+
+    console.log(cart);
+
     return(
-        <CartContext.Provider value={[cart, setCart]}>
-            {children}
-        </CartContext.Provider>
+        <CartContext.Provider value={[cart, setCart, addOn]}>{children}</CartContext.Provider>
     );
 };
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,14 +6,12 @@ import {CartContext} from "../../Context/CartContext.js";
 
 const ItemDetail = ({details}) => {
     const [purchased, setPurchased] = useState(false);
-    const [cart, setCart] = useContext(CartContext);
+    const [cart, setCart, addOn] = useContext(CartContext);
 
     const onAdd = (value) => {
-        setPurchased(true);
-        const item = {quantity: value, item: details.brand+` `+details.model, price: details.price*value}
-        setCart(currentState => [...currentState, item]);
-        console.log(item);
-        console.log(cart);
+        setPurchased(true); 
+        console.log(value);
+        addOn();
     };
 
     return(
@@ -22,7 +20,7 @@ const ItemDetail = ({details}) => {
             <p className="description">{details.description}</p>
             <img src={details.img} alt=""></img>
             <p className="price">{"Precio: $" + details.price}</p>
-            {purchased?(<NavLink to="/cart"><button>Checkout</button></NavLink>):(<ItemCount stock={10} initial={1} onAdd={onAdd} />)}
+            {purchased?(<NavLink to="/cart"><button>Checkout</button></NavLink>):(<ItemCount stock={10} initial={1} onAdd={onAdd} addOn={addOn}/>)}
         </div>
     )
 }
