d7df73f6b4d8780771d3a8db0ef08472fe75ae30
manaqu98 <92610695+manaqu98@users.noreply.github.com>
2022-02-22 10:19:07 -0300

    CartContext Optimization

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -5,15 +5,25 @@ export const CartContext = createContext();
 export const CartProvider = ({children}) => {
     const[cart, setCart] = useState([]);
 
-    const addOn = (details, value) => { 
+    /*const addOn = (details, value) => { 
         const item = {id: details.id, quantity: value, item: details.brand+` `+details.model, price: details.price*value};
         setCart(currentState => [...currentState, item]);
-    };
+    }; --------> Como estaba antes*/
     
     /*const addOn = (details, value) => { 
         const item = {id: details.id, quantity: value, item: details.brand+` `+details.model, price: details.price*value};
-        setCart(currentState => cart.map(currentState => (currentState.id === details.id ? null : [...currentState, item])));
-    }; -----> Intento con parametro de que no agregue repetidos*/
+        setCart(currentState => [...currentState, item]);
+        cart.map(items => items.id === details.id
+            ? console.log(`El item ya existe`)
+            : console.log(`El item no existe, fue agregado`));
+    };------------> Asi funciona correctamente los console.log, pero siempre agrega el producto por la linea 15*/
+
+    const addOn = (details, value) => { 
+        const item = {id: details.id, quantity: value, item: details.brand+` `+details.model, price: details.price*value};
+        cart.map(items => items.id === details.id
+            ? console.log(`El item ya existe`)
+            : setCart(currentState => [...currentState, item]));
+    };/*Intento para que filtre si existe el producto y segun el resultado lo agregue o no*/
 
     console.log(cart);
 
